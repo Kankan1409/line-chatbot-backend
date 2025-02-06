4d96b9ce7390e6cbdb67f6d480bf75d701980f90
Kaohom <kanyarat.melanon@gmail.com>
2025-02-06 21:33:54 +0700

    test:app

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 // ตั้งค่า LINE Bot
-// const config = {
-//     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-//     channelSecret: process.env.CHANNEL_SECRET
-// };
-// const client = new Client(config);
-app.use('/', indexRouter); 
-// app.post('/webhook', middleware(config), (req, res) => {
-//     Promise.all(req.body.events.map(handleEvent))
-//         .then(() => res.sendStatus(200))
-//         .catch(err => console.error(err));
-// });
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+};
+const client = new Client(config);
+// app.use('/', indexRouter); 
+app.post('/webhook', middleware(config), (req, res) => {
+    Promise.all(req.body.events.map(handleEvent))
+        .then(() => res.sendStatus(200))
+        .catch(err => console.error(err));
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
